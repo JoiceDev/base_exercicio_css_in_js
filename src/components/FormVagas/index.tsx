17adb2e036319743391df901f386fd3f6a4d5368
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-09-11 00:29:51 -0300

    conversão da estilização para Styled Components

---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,28 @@
+import { FormEvent, useState } from 'react'
+
+import { Form, BtnPesquisar, Input } from './Styles'
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Form onSubmit={aoEnviarForm}>
+      <Input
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <BtnPesquisar type="submit">Pesquisar</BtnPesquisar>
+    </Form>
+  )
+}
+export default FormVagas
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import { HeroForm, HeroTitle } from './Styles'
+
+const Hero = () => (
+  <HeroForm>
+    <div>
+      <HeroTitle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </div>
+  </HeroForm>
+)
+
+export default Hero
